2a26abf71b5f85781f2f2d2e3f16ee879c907250
Cloudymolecule <baires82@gmail.com>
2021-04-25 17:35:36 -0400

    commit tree f1a3a69acee415b751fa718c297ad7ce465d170c

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    
+    state = {
+        people: []
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json').then(res => res.json()).then(data => {this.setState({people: data.people})})
+    }
+}
