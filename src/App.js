e85c50ab9dda581dd424fad32c4d97433e240540
Cloudymolecule <baires82@gmail.com>
2021-04-25 17:41:19 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    
+    state = {
+        people: []
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>People in Space: </h1>
+                {this.state.people.map(p => <li>{p.name}</li>)}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json').then(res => res.json()).then(data => {this.setState({people: data.people})})
+    }
+}
